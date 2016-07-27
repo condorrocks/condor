e93a9be88daf6874581e0b74481a92a6c4847e17
Ariel Vallese <alariva@gmail.com>
2016-07-26 21:39:59 -0300

    Schedule uptime:feed hourly command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -20,11 +20,11 @@ class Kernel extends ConsoleKernel
      * Define the application's command schedule.
      *
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     *
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command('uptime:feed')->hourly();
     }
 }
