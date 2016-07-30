bc432c3838f9282d85cacd689aacba3540691187
Ariel Vallese <alariva@gmail.com>
2016-07-29 21:14:59 -0300

    Add tests for snapshot management

---
+++ tests/integration/Http/Controllers/Manage/ManageSnapshotControllerTest.php
@@ -0,0 +1,66 @@
+<?php
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+
+class ManageSnapshotControllerTest extends TestCase
+{
+    use DatabaseTransactions;
+    use WithoutMiddleware;
+    use CreateUser, CreateAccount, CreateBoard, CreateSnapshot;
+
+    /**
+     * @var App\User
+     */
+    protected $user;
+
+    /**
+     * @var App\Account
+     */
+    protected $account;
+
+    /**
+     * @var App\Board
+     */
+    protected $board;
+
+    /**
+     * @var App\Snapshot
+     */
+    protected $snapshot;
+
+    /** @test */
+    public function it_purges_snapshots_from_board()
+    {
+        $this->scenario();
+
+        $this->actingAs($this->user);
+
+        $this->seeInDatabase('snapshots', ['target' => $this->snapshot->target]);
+
+        $this->visit(route('manage.boards.purge', $this->board));
+
+        $this->dontSeeInDatabase('snapshots', ['target' => $this->snapshot->target]);
+    }
+
+    //////////////////////
+    // Scenario Helpers //
+    //////////////////////
+
+    protected function scenario()
+    {
+        $this->user = $this->createUser();
+
+        $this->account = $this->createAccount();
+
+        $this->user->accounts()->save($this->account);
+
+        $this->board = $this->createBoard();
+
+        $this->account->boards()->save($this->board);
+
+        $this->snapshot = $this->createSnapshot();
+
+        $this->board->snapshots()->save($this->snapshot);
+    }
+}
