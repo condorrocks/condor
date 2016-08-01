3062fbca57831ea407ed35f4059d75ceeffc9a8a
Ariel Vallese <alariva@gmail.com>
2016-08-01 16:58:35 -0300

    Enhance code

--- app/Snapshot.php
+++ app/Snapshot.php
@@ -4,6 +4,17 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property int $board_id
+ * @property BelongsTo|Board $board
+ * @property int $aspect_id
+ * @property BelongsTo|Aspect $aspect
+ * @property string $hash
+ * @property string $target
+ * @property Carbon $timestamp
+ * @property string $data
+ */
 class Snapshot extends Model
 {
     /**
@@ -14,6 +25,13 @@ class Snapshot extends Model
     protected $fillable = [
         'board_id', 'aspect_id', 'hash', 'target', 'timestamp', 'data',
     ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['timestamp'];
 
     public function board()
     {
