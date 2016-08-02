4cb696235ccb17aba99ce033a048710dcf02ec5d
Ariel Vallese <alariva@gmail.com>
2016-08-02 17:06:31 -0300

    Configure mailing

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => env('MAIL_FROM', 'notifications@condor.rocks'), 'name' => env('MAIL_NAME', 'Condor')],
 
     /*
     |--------------------------------------------------------------------------
